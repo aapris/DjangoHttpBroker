92cff22c68c4e0a563e45ed722db6ea5eac5feed
Aapo Rista <aapo.gitpub@rista.net>
2019-01-07 14:33:29 +0200

    Add @csrf_exempt to catchall view

--- plugindemo/businesslogic/views.py
+++ plugindemo/businesslogic/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
 from .models import Endpoint
 from .endpoint import EndpointProvider
 
 
+@csrf_exempt
 def catchall(request, path):
     """
     Find a right handler for requested path.
