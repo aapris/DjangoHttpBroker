db87e61da8b8d7c41487d17212d3b20badf71013
Aapo Rista <aapo.gitpub@rista.net>
2019-01-07 14:32:32 +0200

    Add dummy endpoint and forward

---
+++ plugindemo/internalplugin/endpoints/dummy.py
@@ -0,0 +1,12 @@
+from django.http.response import HttpResponse
+from businesslogic.endpoint import EndpointProvider
+
+
+class DummyEndpoint(EndpointProvider):
+    description = 'Dummy endpoint, which does nothing but just returns OK'
+
+    def handle_request(self, request):
+        # For some reason request.body must be accessed, otherwise there will occur mysterious
+        # second request with post payload, at least in development server
+        _ = request.body
+        return HttpResponse('OK', content_type='text/plain')
