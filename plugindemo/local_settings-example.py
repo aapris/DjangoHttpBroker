42317d92d4d67f606535e06978cd5d0c36c56c50
Aapo Rista <aapo.gitpub@rista.net>
2019-01-20 12:44:41 +0200

    Add INSTALLED_APPS

--- plugindemo/local_settings-example.py
+++ plugindemo/local_settings-example.py
@@ -1,10 +1,24 @@
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'paxcounter',  # Or path to database file if using sqlite3.
-        'USER': 'arista',  # Not used with sqlite3.
+        'NAME': 'dbname',  # Or path to database file if using sqlite3.
+        'USER': 'username',  # Not used with sqlite3.
         'PASSWORD': '',  # Not used with sqlite3.
         'HOST': '',  # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',  # Set to empty string for default. Not used with sqlite3.
     }
 }
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'businesslogic',  # Contains URL routing, model for
+    'internalplugin',  # Just some default endpoints for demonstration purposes
+    'thirdparty',  # Some endpoints from a third party provider
+]
