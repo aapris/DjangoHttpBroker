868c8c63f263a302f152a5766e46e9031fc1ad39
Aapo Rista <aapo.gitpub@rista.net>
2019-02-19 17:42:36 +0200

    Comment out informative (but verbose) prints

--- httpbroker/broker/setup.py
+++ httpbroker/broker/setup.py
@@ -9,26 +9,28 @@ def import_plugins():
     Import all plugin modules and initialise EndpointProvider instances found.
     """
     apps = []
-    print('Loading endpoints...')
+    # print('Loading endpoints...')
 
     for appname in settings.INSTALLED_APPS:
         if appname.startswith('django.') is False:
-            print(f'{appname}: ', end='')
+            # print(f'{appname}: ', end='')
             try:
                 apps.append(import_module(appname + '.endpoints'))
-                print('endpoints found!')
+                # print('endpoints found!')
             except ModuleNotFoundError as err:
-                print(err)
+                pass
+                # print(err)
             try:
                 apps.append(import_module(appname + '.forwards'))
-                print('forwards found!')
+                # print('forwards found!')
             except ModuleNotFoundError as err:
-                print(err)
+                pass
+                # print(err)
 
-        endpoints = EndpointProvider.get_plugins()
-        for e in endpoints:
-            print(f'{e.name}: {e.description}')
-
-        forwards = ForwardProvider.get_plugins()
-        for f in forwards:
-            print(f'{f.name}: {f.description}')
+        # endpoints = EndpointProvider.get_plugins()
+        # for e in endpoints:
+        #     print(f'{e.name}: {e.description}')
+        #
+        # forwards = ForwardProvider.get_plugins()
+        # for f in forwards:
+        #     print(f'{f.name}: {f.description}')
