21b354c04fc8a16104b4eaaf446cb4c24fdbacb0
Aapo Rista <aapo.gitpub@rista.net>
2019-02-19 15:05:25 +0200

    More verbose listing

--- httpbroker/broker/management/commands/list_endpoints.py
+++ httpbroker/broker/management/commands/list_endpoints.py
@@ -1,5 +1,7 @@
+import json
 from django.core.management.base import BaseCommand, CommandError
-from broker.models import Endpoint
+from broker.models import Endpoint, Forward
+from broker.endpoint import EndpointProvider, ForwardProvider
 
 
 class Command(BaseCommand):
@@ -10,5 +12,22 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args, **options):
+        print('Available Endpoints\n=========================')
+        endpoints = EndpointProvider.get_plugins()
+        for a in endpoints:
+            name = f'{a.app}.{a.name}'
+            print(f'{name: <40}{a.description}')
+        print('\nConfigured Endpoints\n=========================')
         for e in Endpoint.objects.all():
-            print(f'/{e.path}')
+            print(f'{e.handler: <40}/{e.path}')
+
+        print('\nAvailable Forwards\n=========================')
+        forwards = ForwardProvider.get_plugins()
+        # forwards.sort()
+        for a in forwards:
+            name = f'{a.app}.{a.name}'
+            print(f'{name: <40}{a.description}')
+        print('\nConfigured Forwards\n=========================')
+        for e in Forward.objects.all():
+            config = json.dumps(json.loads(e.config))
+            print(f'{e.handler: <40}{config}')
