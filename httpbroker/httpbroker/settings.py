e330188fd072a4b4c9f7ce873fa45f14bb2ca205
Aapo Rista <aapo.gitpub@rista.net>
2019-02-21 16:18:44 +0200

    Add CELERY_* and VAR_DIR

--- httpbroker/httpbroker/settings.py
+++ httpbroker/httpbroker/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+VAR_DIR = os.path.join(BASE_DIR, 'var')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -115,6 +116,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# CELERY
+CELERY_BROKER_URL = 'amqp://localhost'  # RabbitMQ on localhost
+CELERY_ACCEPT_CONTENT = ['msgpack']
+CELERY_TASK_SERIALIZER = 'msgpack'  # Packs bytes (json won't)
 
 # local_settings.py can be used to override environment-specific settings
 # like database and email that differ between development and production.
