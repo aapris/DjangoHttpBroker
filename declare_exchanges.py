ad0c11ed3d167015ab46c4812038066814f14642
Aapo Rista <aapo.gitpub@rista.net>
2019-02-26 17:28:22 +0200

    Add parsed data exchange

--- declare_exchanges.py
+++ declare_exchanges.py
@@ -1,6 +1,7 @@
 import pika
 
 RAW_HTTP_EXCHANGE = 'incoming_raw_http'
+PARSED_DATA_EXCHANGE = 'outgoing_parsed_data'
 
 connection = pika.BlockingConnection(
     pika.ConnectionParameters('localhost', 5672, '/',
@@ -8,8 +9,16 @@ connection = pika.BlockingConnection(
                               ))
 
 channel = connection.channel()
+
 channel.exchange_declare(
     exchange=RAW_HTTP_EXCHANGE,
+    exchange_type='topic',
+    durable=True,
+    auto_delete=False,
+)
+
+channel.exchange_declare(
+    exchange=PARSED_DATA_EXCHANGE,
     exchange_type='topic',
     durable=True,
     auto_delete=False,
