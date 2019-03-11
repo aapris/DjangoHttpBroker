50818ec76229a15ea06b31b5ec3cc654fd34cfef
Aapo Rista <aapo.gitpub@rista.net>
2019-03-11 21:08:49 +0200

    Add new helper functions

--- httpbroker/broker/utils.py
+++ httpbroker/broker/utils.py
@@ -6,10 +6,14 @@ values out from HttpRequest.META and returns them in a dict which is json, msgpa
 serializable.
 """
 
+import base64
 import msgpack
+import json
 import pika
 import pika.exceptions
 from django.conf import settings
+from django.contrib.auth import authenticate
+from django.utils import timezone
 
 META_STARTSWITH = ('SERVER', 'REMOTE')  # REMOTE_ADDR etc.
 META_EXACT = ('QUERY_STRING', 'REQUEST_METHOD', 'SCRIPT_NAME', 'PATH_INFO')
@@ -78,3 +82,73 @@ def send_message(exchange, key, message):
                                                           delivery_mode=2))
     channel.close()
     connection.close()
+
+
+def basicauth(request):
+    """Check for valid basic auth header."""
+    uname, passwd, user = None, None, None
+    if 'HTTP_AUTHORIZATION' in request.META:
+        auth = request.META['HTTP_AUTHORIZATION'].split()
+        if len(auth) == 2:
+            if auth[0].lower() == "basic":
+                a = auth[1].encode('utf8')
+                s = base64.b64decode(a)
+                uname, passwd = s.decode('utf8').split(':')
+                user = authenticate(username=uname, password=passwd)
+    return uname, passwd, user
+
+
+def decode_json_body(body):
+    body_str = body.decode('utf8', "backslashreplace")
+    try:
+        data = json.loads(body_str)
+        return True, data
+    except json.decoder.JSONDecodeError as err:
+        return False, err
+
+
+def get_datalogger(devid, name='', update_activity=False):
+    # FIXME: Shit, this import can't be in the beginning of the file or we'll get:
+    # "django.core.exceptions.AppRegistryNotReady: Apps aren't loaded yet."
+    # It can be imported if you run django.setup() before import, but it messes up loading
+    # import django
+    # django.setup()
+    # from broker.models import Datalogger
+    from .models import Datalogger
+
+    datalogger, created = Datalogger.objects.get_or_create(devid=devid)
+    changed = False
+    if created:
+        datalogger.name = name
+        changed = True
+    if update_activity:
+        datalogger.activity_at = timezone.now()
+        changed = True
+    if changed:
+        datalogger.save()
+    return datalogger, created
+
+
+def validate_parsed_data_message(message, validate_data=False):
+    """
+    Check that message has valid parsed data structure
+    :param dict message: data in parsed data format
+    :param bool validate_data: check data values if True
+    :return: bool valid or not
+    """
+    if 'devid' not in message:
+        return False
+    if 'datalines' not in message:
+        return False
+    datalines = message['datalines']
+    if isinstance(datalines, list) is False:
+        return False
+    for line in datalines:
+        if 'time' not in line or 'data' not in line:
+            return False
+        if validate_data:
+            # TODO: implement these
+            # parse time
+            # parse data values (are floats)
+            pass
+    return True
