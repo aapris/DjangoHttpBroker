1164ea22302d1469584e7f8cd076e656b8c8082f
Aapo Rista <aapo.gitpub@rista.net>
2019-03-11 21:08:03 +0200

    Add Datalogger and DataloggerForward models

--- httpbroker/broker/admin.py
+++ httpbroker/broker/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Endpoint, Forward, Decoder
+from .models import Datalogger, DataloggerForward
 
 
 class EndpointAdmin(admin.ModelAdmin):
@@ -21,3 +22,16 @@ class DecoderAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Decoder, DecoderAdmin)
+
+
+class ForwardInline(admin.StackedInline):
+    model = DataloggerForward
+    extra = 1
+
+
+class DataloggerAdmin(admin.ModelAdmin):
+    inlines = [ForwardInline]
+    list_display = ('devid', 'name', 'activity_at', 'created_at')
+
+
+admin.site.register(Datalogger, DataloggerAdmin)
--- httpbroker/broker/models.py
+++ httpbroker/broker/models.py
@@ -51,3 +51,42 @@ class Decoder(JsonConfigModel):
 
     def __str__(self):
         return '{}'.format(self.handler)
+
+
+class Datalogger(models.Model):
+    devid = models.CharField(db_index=True, unique=True, max_length=256)
+    name = models.CharField(max_length=256, blank=True)
+    description = models.CharField(max_length=10000, blank=True)
+    decoder = models.CharField(max_length=128, blank=True, choices=DECODER_HANDLER_CHOICES)
+    forwards = models.ManyToManyField(Forward,
+                                      blank=True,
+                                      through="DataloggerForward",
+                                      related_name="related_dataloggers",
+                                      verbose_name="Data to forward")
+    lat = models.FloatField(blank=True, null=True)
+    lon = models.FloatField(blank=True, null=True)
+    country = models.CharField(max_length=256, blank=True)
+    locality = models.CharField(max_length=256, blank=True)
+    street = models.CharField(max_length=256, blank=True)
+    activity_at = models.DateTimeField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.devid} ({self.decoder})'
+
+
+class DataloggerForward(models.Model):
+    datalogger = models.ForeignKey(Datalogger, on_delete=models.CASCADE)
+    forward = models.ForeignKey(Forward, on_delete=models.CASCADE, related_name='related_forwards')
+    config = models.TextField(default='{}', help_text='All required configuration parameters in JSON format')
+
+    def __str__(self):
+        return f'{self.datalogger} -> {self.forward}'
+
+    def clean(self, exclude=None):
+        try:
+            config = json.loads(self.config)
+        except json.JSONDecodeError as err:
+            raise ValidationError(f'JSON error in config: {err}')
+        self.config = json.dumps(config, indent=1)
