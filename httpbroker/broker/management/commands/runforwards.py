77421f34bf2cbf0305ac19b6c7783d3bcf8f281c
Aapo Rista <aapo.gitpub@rista.net>
2019-03-29 18:49:09 +0200

    Add command which executes all Forwards configured for this datalogger

---
+++ httpbroker/broker/management/commands/runforwards.py
@@ -0,0 +1,51 @@
+import json
+import logging
+
+from django.conf import settings
+
+from broker.management.commands import RabbitCommand
+from broker.providers.forward import ForwardProvider
+from broker.utils import data_unpack, get_datalogger
+
+logger = logging.getLogger('django')
+
+
+def consumer_callback(channel, method, properties, body, options=None):
+    """
+    Extract data from RabbitMQ message's body and save sensor values into InfluxDB
+    """
+    parsed_data = data_unpack(body)
+    devid = parsed_data['devid']
+    logger.debug(f'Handling {devid}')
+    datalogger, created = get_datalogger(devid=devid, update_activity=False)
+    dataloggerforwards = datalogger.dataloggerforwards.all()
+    plugins = ForwardProvider.get_plugins({})
+
+    for dlf in dataloggerforwards:
+        # Read Forward instance's config
+        f_config = json.loads(dlf.forward.config)
+        # Read logger's DataForward instance's config
+        config = json.loads(dlf.config)
+        # Override fields defined in dataloggerForward's config
+        f_config.update(config)
+        # Find correct plugin and call its forward_data() function
+        for plugin in plugins:
+            forward_handler = f'{plugin.app}.{plugin.name}'
+            if forward_handler == dlf.forward.handler:
+                plugin.forward_data(datalogger, parsed_data, f_config)
+    channel.basic_ack(method.delivery_tag)
+
+
+class Command(RabbitCommand):
+    help = 'Read RabbitMQ queue and save all data into InfluxDB'
+
+    def add_arguments(self, parser):
+        super().add_arguments(parser)
+
+    def handle(self, *args, **options):
+        logger.info(f'Start handling {__name__}')
+        options['exchange'] = settings.PARSED_DATA_EXCHANGE
+        options['queue'] = 'runforwards_queue'
+        options['routing_key'] = f'{settings.RABBITMQ["ROUTING_KEY_PREFIX"]}.#'
+        options['consumer_callback'] = consumer_callback
+        super().handle(*args, **options)
