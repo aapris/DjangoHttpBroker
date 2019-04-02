bdb0a2909c22b64adb8552d023f027d71e747ddf
Aapo Rista <aapo.gitpub@rista.net>
2019-04-02 23:31:39 +0300

    Implement InfluxDB Forward

--- httpbroker/broker/forwards/influxdb.py
+++ httpbroker/broker/forwards/influxdb.py
@@ -1,4 +1,7 @@
+from influxdb.exceptions import InfluxDBClientError
+from dateutil.parser import parse
 from broker.providers.forward import ForwardProvider
+from thingpark.utils import create_influxdb_obj, get_influxdb_client
 
 
 class InfluxDBForward(ForwardProvider):
@@ -8,6 +11,26 @@ class InfluxDBForward(ForwardProvider):
         super().__init__(args, kwargs)
 
     def forward_data(self, datalogger, data, config):
-        # This does nothing yet, since saving into InfluxFB
-        # is done in management command save2influxdb
+        """
+        Insert parsed data into InfluxDB database.
+        influxdb_measurement and influxdb_database MUST be defined in Forward's or DataloggerForward's config.
+        
+        :param Datalogger datalogger:
+        :param dict data:
+        :param dict config:
+        :return: True on success, False if writing data failed
+        """
+        measurement_name = config['influxdb_measurement']
+        dbname = config['influxdb_database']
+        measurements = []
+        for dataline in data['datalines']:
+            ts = parse(dataline['time'])
+            measurement = create_influxdb_obj(datalogger.devid, measurement_name, dataline['data'], ts)
+            measurements.append(measurement)
+        iclient = get_influxdb_client(database=dbname)
+        iclient.create_database(dbname)
+        try:
+            iclient.write_points(measurements)
+        except InfluxDBClientError as err:
+            return False
         return True
