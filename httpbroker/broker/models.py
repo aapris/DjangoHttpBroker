33393c18333c2481f4f6439d30677919c1fe89cc
Aapo Rista <aapo.gitpub@rista.net>
2019-05-17 09:42:23 +0300

    Get choices from callable

--- httpbroker/broker/models.py
+++ httpbroker/broker/models.py
@@ -1,23 +1,27 @@
 import json
+
+from django.core.exceptions import ValidationError
 from django.db import models
+
+from broker.setup import import_plugins
+from .providers.decoder import DecoderProvider
 from .providers.endpoint import EndpointProvider
 from .providers.forward import ForwardProvider
-from .providers.decoder import DecoderProvider
-from django.core.exceptions import ValidationError
-from broker.setup import import_plugins
 
 import_plugins()
-endpoints = EndpointProvider.get_plugins()
-ENDPOINT_HANDLER_CHOICES = [(f'{a.app}.{a.name}', f'{a.app}.{a.name}') for a in endpoints]
-forwards = ForwardProvider.get_plugins()
-FORWARD_HANDLER_CHOICES = [(f'{a.app}.{a.name}', f'{a.app}.{a.name}') for a in forwards]
-decoders = DecoderProvider.get_plugins()
-DECODER_HANDLER_CHOICES = [(f'{a.app}.{a.name}', f'{a.app}.{a.name}') for a in decoders]
+
+
+# TODO: reset all migrations
+# https://simpleisbetterthancomplex.com/tutorial/2016/07/26/how-to-reset-migrations.html
+
+def get_handler_choices(Model):
+    plugins = Model.get_plugins()
+    return [(f'{p.app}.{p.name}', f'{p.app}.{p.name}') for p in plugins]
 
 
 class JsonConfigModel(models.Model):
     name = models.CharField(max_length=256, blank=True)
-    description = models.CharField(max_length=10000, blank=True)
+    description = models.TextField(max_length=10000, blank=True)
     config = models.TextField(default='', help_text='All required configuration parameters in JSON format')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -35,28 +39,29 @@ class JsonConfigModel(models.Model):
 
 class Endpoint(JsonConfigModel):
     path = models.CharField(db_index=True, max_length=256)
-    handler = models.CharField(max_length=64, choices=ENDPOINT_HANDLER_CHOICES)
+    handler = models.CharField(max_length=64, choices=get_handler_choices(EndpointProvider))
 
     def __str__(self):
-        return '{}'.format(self.path)
         return '{} / {} ({})'.format(self.path, self.handler, self.name)
 
 
 class Forward(JsonConfigModel):
-    handler = models.CharField(max_length=64, choices=FORWARD_HANDLER_CHOICES)
+    handler = models.CharField(max_length=64, choices=get_handler_choices(ForwardProvider))
 
     def __str__(self):
         return '{} ({})'.format(self.handler, self.name)
 
 
+# TODO: check this, seems a bit obsolete
 class Decoder(JsonConfigModel):
-    handler = models.CharField(max_length=64, choices=DECODER_HANDLER_CHOICES)
+    handler = models.CharField(max_length=64, choices=get_handler_choices(DecoderProvider))
 
     def __str__(self):
         return '{} ({})'.format(self.handler, self.name)
 
 
 class Application(JsonConfigModel):
+    decoder = models.CharField(max_length=128, blank=True, choices=get_handler_choices(DecoderProvider))
 
     def __str__(self):
         return '{}'.format(self.name)
@@ -67,7 +72,7 @@ class Datalogger(models.Model):
     application = models.ForeignKey(Application, null=True, on_delete=models.SET_NULL, related_name='dataloggers')
     name = models.CharField(max_length=256, blank=True)
     description = models.CharField(max_length=10000, blank=True)
-    decoder = models.CharField(max_length=128, blank=True, choices=DECODER_HANDLER_CHOICES)
+    decoder = models.CharField(max_length=128, blank=True, choices=get_handler_choices(DecoderProvider))
     forwards = models.ManyToManyField(Forward,
                                       blank=True,
                                       through="DataloggerForward",
