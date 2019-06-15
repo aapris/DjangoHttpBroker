4ab258f5dbee813a771b7e5476758c2ba2ab8fd4
Aapo Rista <aapo.gitpub@rista.net>
2019-06-15 16:23:39 +0200

    Move InfluxDB functions to utility module

--- httpbroker/broker/management/commands/save2influxdb.py
+++ httpbroker/broker/management/commands/save2influxdb.py
@@ -1,56 +1,12 @@
 import json
-import influxdb
 import logging
 from django.conf import settings
 from influxdb.exceptions import InfluxDBClientError
 from broker.management.commands import RabbitCommand
 from broker.utils import data_unpack, get_datalogger
+from broker.utils.influxdb import get_influxdb_client, create_influxdb_objects
 
 logger = logging.getLogger('django')
-
-
-def get_influxdb_client(host='127.0.0.1', port=8086, database='mydb'):
-    iclient = influxdb.InfluxDBClient(host=host, port=port, database=database)
-    return iclient
-
-
-def create_influxdb_objects(data, measurement_name, extratags=None):
-    """
-    data =
-    {
-        "devid": "373773207E33011C",
-        "datalines": [
-            {
-                "time": "2019-03-11T12:49:09.239162+00:00",
-                "data": {
-                    'pm25min': 0.2, 'pm25max': 0.3, 'pm25avg': 0.2, 'pm25med': 0.2,
-                    'pm10min': 0.2, 'pm10max': 1.3, 'pm10avg': 0.7, 'pm10med': 0.6,
-                    'temp': 22.5, 'humi': 22.0, 'pres': 1021.6, 'gas': 826.6,
-                    'rssi': '-81.000000'
-                }
-            }
-        ]
-    }
-    :param dict data: Data object, see example above
-    :param str measurement_name: Measurement's name
-    :param dict extratags: Additional InfluxDB measurement tags
-    :return:
-    """
-    devid = data['devid']
-    measurements = []
-    for d in data['datalines']:
-        measurement_obj = {
-            "measurement": measurement_name,
-            "tags": {
-                "dev-id": devid,
-            },
-            "time": d['time'],
-            "fields": d['data']
-        }
-        if extratags is not None:
-            measurement_obj['tags'].update(extratags)
-        measurements.append(measurement_obj)
-    return measurements
 
 
 def datalogger_get_config(datalogger, parsed_data):
---
+++ httpbroker/broker/utils/influxdb.py
@@ -0,0 +1,67 @@
+import json
+import logging
+
+import influxdb
+from influxdb.exceptions import InfluxDBClientError
+
+logger = logging.getLogger('django')
+
+
+def get_influxdb_client(host='127.0.0.1', port=8086, database='mydb'):
+    iclient = influxdb.InfluxDBClient(host=host, port=port, database=database)
+    return iclient
+
+
+def create_influxdb_objects(data, measurement_name, extratags=None):
+    """
+    Convert a "parsed data object" (example below) to an InfluxDB object.
+
+    data =
+    {
+        "devid": "373773207E33011C",
+        "datalines": [
+            {
+                "time": "2019-03-11T12:49:09.239162+00:00",
+                "data": {
+                    'pm25min': 0.2, 'pm25max': 0.3, 'pm25avg': 0.2, 'pm25med': 0.2,
+                    'pm10min': 0.2, 'pm10max': 1.3, 'pm10avg': 0.7, 'pm10med': 0.6,
+                    'temp': 22.5, 'humi': 22.0, 'pres': 1021.6, 'gas': 826.6,
+                    'rssi': '-81.000000'
+                }
+            }
+        ]
+    }
+    :param dict data: Data object, see example above
+    :param str measurement_name: Measurement's name
+    :param dict extratags: Additional InfluxDB measurement tags
+    :return: InfluxDB measurement object
+    """
+    devid = data['devid']
+    measurements = []
+    for d in data['datalines']:
+        measurement_obj = {
+            "measurement": measurement_name,
+            "tags": {
+                "dev-id": devid,
+            },
+            "time": d['time'],
+            "fields": d['data']
+        }
+        if extratags is not None:
+            measurement_obj['tags'].update(extratags)
+        measurements.append(measurement_obj)
+    return measurements
+
+
+def save_parsed_data2influxdb(db_name, measurement_name, parsed_data):
+    devid = parsed_data['devid']
+    measurements = create_influxdb_objects(parsed_data, measurement_name)
+    logger.debug(json.dumps(measurements))
+    iclient = get_influxdb_client(database=db_name)
+    if not any(db['name'] == db_name for db in iclient.get_list_database()):
+        iclient.create_database(db_name)
+    try:
+        iclient.write_points(measurements)
+    except InfluxDBClientError as err:
+        err_msg = f'InfluxDB error: {err}'
+        logger.error(f'devid={devid} {err_msg}')
