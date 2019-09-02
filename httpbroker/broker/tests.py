64196acbd3190ea15f49c125a911375a279af08d
Aapo Rista <aapo.gitpub@rista.net>
2019-09-02 16:39:16 +0300

    Add first tests

--- httpbroker/broker/tests.py
+++ httpbroker/broker/tests.py
@@ -1,3 +1,30 @@
+import json
+from io import StringIO
+
+from django.core import management
+from django.test import Client
 from django.test import TestCase
 
-# Create your tests here.
+from broker.models import Endpoint
+
+ENDPOINT_PATH = 'default'
+
+
+class EndpointTestCase(TestCase):
+    def setUp(self):
+        Endpoint.objects.create(path=ENDPOINT_PATH, handler='broker.DefaultEndpoint')
+
+    def test_endpoints(self):
+        """Test default endpoint and how request data is stored to the filesystem"""
+        c = Client()
+
+        # Test endpoint
+        response = c.get(f'/{ENDPOINT_PATH}')
+        self.assertEqual(response.status_code, 200)
+
+        # Test data which was dumped to the disk during request
+        out = StringIO()
+        devid = f'unknown-{ENDPOINT_PATH}'
+        management.call_command('extract_message', '--devid', devid, stdout=out)
+        request_data = json.loads(out.getvalue())
+        self.assertEqual(request_data['devid'], devid)
